e69f9e211bc1c83122db6861292e05406c4b0f83
ryan.ch <ryan.ch@alibaba-inc.com>
2018-10-30 16:07:02 +0800

    train query_data

--- train_bert_lm.py
+++ train_bert_lm.py
@@ -25,7 +25,7 @@ FLAGS=tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_boolean("test_mode",False,"whether it is test mode. if it is test mode, only small percentage of data will be used")
 tf.app.flags.DEFINE_string("data_path","./data/","path of traning data.")
-tf.app.flags.DEFINE_string("mask_lm_source_file","./data/bert_train.txt","path of traning data.")
+tf.app.flags.DEFINE_string("mask_lm_source_file","./data/just_query.csv","path of traning data.")
 tf.app.flags.DEFINE_string("ckpt_dir","./checkpoint_lm/","checkpoint location for the model") #save to here, so make it easy to upload for test
 tf.app.flags.DEFINE_integer("vocab_size",60000,"maximum vocab size.")
 tf.app.flags.DEFINE_integer("d_model", 64, "dimension of model") # 512-->128
@@ -41,7 +41,7 @@ tf.app.flags.DEFINE_integer("batch_size", 64, "Batch size for training/evaluatin
 tf.app.flags.DEFINE_integer("decay_steps", 1000, "how many steps before decay learning rate.")
 tf.app.flags.DEFINE_float("decay_rate", 1.0, "Rate of decay for learning rate.")
 tf.app.flags.DEFINE_float("dropout_keep_prob", 0.9, "percentage to keep when using dropout.")
-tf.app.flags.DEFINE_integer("sequence_length",200,"max sentence length")#400
+tf.app.flags.DEFINE_integer("sequence_length",100,"max sentence length")#400
 tf.app.flags.DEFINE_integer("sequence_length_lm",10,"max sentence length for masked language model")
 tf.app.flags.DEFINE_boolean("is_training",True,"is training.true:tranining,false:testing/inference")
 tf.app.flags.DEFINE_boolean("is_fine_tuning",False,"is_finetuning.ture:this is fine-tuning stage")
